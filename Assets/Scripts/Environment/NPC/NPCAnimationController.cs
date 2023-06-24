cf74fc117c125c0b9c948a133e17453403fd62ba
Pojemnik <andriej194@gmail.com>
2023-06-24 23:36:27 +0200

    Fix typo in class name

--- Assets/Scripts/Environment/NPC/NPCAnimationController.cs
+++ Assets/Scripts/Environment/NPC/NPCAnimationController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animation))]
+[RequireComponent(typeof(Animator))]
 public class NPCAnimationController : MonoBehaviour
 {
     [SerializeField]
