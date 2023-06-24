530bae8f778f4502310c61135148ac95605424a6
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2023-06-24 15:52:29 +0200

    make turn-ip

--- Assets/Scripts/Minigames/SpamLeftRight/SpamLeftRightLogic.cs
+++ Assets/Scripts/Minigames/SpamLeftRight/SpamLeftRightLogic.cs
@@ -9,10 +9,20 @@ public class SpamLeftRightLogic : MonoBehaviour
     private int requiredClicks = 10;
     [SerializeField]
     private float timeLimit = 10;
+    [SerializeField]
+    private float angle = 20;
+    [SerializeField]
+    private UIBarController timer;
+    [SerializeField]
+    private UIBarController clicksCounter;
+    [SerializeField]
+    private RectTransform turnip;
     public event EventHandler OnWinEvent;
     public event EventHandler OnLoseEvent;
     private int currentClicks = 0;
     private wasd keyToClick;
+    private float startAngle;
+
     private enum wasd
     {
         w, a, s, d
@@ -23,6 +33,9 @@ public class SpamLeftRightLogic : MonoBehaviour
         if (key == keyToClick.ToString())
         {
             currentClicks++;
+            clicksCounter.ChangeValue(currentClicks, requiredClicks);
+            turnip.rotation = Quaternion.Euler(0, 0, (keyToClick == wasd.a ? angle : -angle) + startAngle);
+
             keyToClick = keyToClick == wasd.a ? wasd.d : wasd.a;
             Debug.Log(currentClicks);
             if (currentClicks >= requiredClicks)
@@ -36,14 +49,19 @@ public class SpamLeftRightLogic : MonoBehaviour
     private void Start()
     {
         keyToClick = wasd.a;
+        startAngle = turnip.rotation.eulerAngles.z;
+        clicksCounter.ChangeValue(0, 1);
+        timer.ChangeValueInverted(0, 1);
         StartCoroutine(TimeCourutine());
     }
 
     private IEnumerator TimeCourutine()
     {
-        for (int i = 0; i <= timeLimit; i++)
+        float counter = 0;
+        while ((counter += Time.deltaTime) < timeLimit)
         {
-            yield return new WaitForSeconds(1);
+            timer.ChangeValueInverted(counter, timeLimit);
+            yield return null;
         }
         OnLoseEvent?.Invoke(this, null);
     }
