f73d8b37b1882f38c2ceb19e6c108f76be263dbb
Pojemnik <andriej194@gmail.com>
2023-06-24 09:45:23 +0200

    Add showing menu on button press

--- Assets/Scripts/UI/UIMenuVisibilityManager.cs
+++ Assets/Scripts/UI/UIMenuVisibilityManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIMenuVisibilityManager : MonoBehaviour
@@ -7,13 +8,28 @@ public class UIMenuVisibilityManager : MonoBehaviour
     [SerializeField]
     private GameObject menuObject;
 
+    private bool menuShown;
+
+    private void Start()
+    {
+        HideMenu();
+    }
+
     public void ShowMenu()
     {
+        menuShown = true;
         menuObject.SetActive(true);
     }
 
     public void HideMenu()
     {
+        menuShown = false;
         menuObject.SetActive(false);
+    }
+
+    public void SwitchMenu()
+    {
+        menuShown = !menuShown;
+        menuObject.SetActive(menuShown);
     }
 }
