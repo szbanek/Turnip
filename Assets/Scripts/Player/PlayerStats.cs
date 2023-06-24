0e1262cb544c7cdc90dfa7451b5c00c06dd85d62
Pojemnik <andriej194@gmail.com>
2023-06-24 09:33:45 +0200

    Add player stats

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float MaxStamina;
+    public float StaminaRegen;
+    public float SprintSpeed;
+    public float JumpCost;
+    public bool HasSense;
+    public float SenseRange;
+    public float AdditionalVegetableChance;
+    public float MinigameBonus;
+    public float TradeBonus;
+}
