420720b7d0e534f2ec8374ccfe68bab7b913c06e
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2023-06-25 16:12:12 +0200

    add intro

--- Assets/Scripts/Misc/MainMenuUtils.cs
+++ Assets/Scripts/Misc/MainMenuUtils.cs
@@ -7,6 +7,15 @@ using UnityEngine.SceneManagement;
 public class MainMenuUtils : MonoBehaviour
 {
     [SerializeField]
+    private GameObject newGameButton;
+    [SerializeField]
+    private GameObject creditsButton;
+    [SerializeField]
+    private GameObject intro;
+    [SerializeField]
+    private GameObject startButton;
+
+    [SerializeField]
     private GameObject credits;
     public void LoadScene()
     {
@@ -16,5 +25,14 @@ public class MainMenuUtils : MonoBehaviour
     public void ShowCredits()
     {
         credits.SetActive(!credits.activeSelf);
+    }
+
+    public void NewGame()
+    {
+        newGameButton.SetActive(false);
+        creditsButton.SetActive(false);
+        credits.SetActive(false);
+        startButton.SetActive(true);
+        intro.SetActive(true);
     }
 }
