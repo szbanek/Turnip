a0a1ebcd750633efd4d57a284c1d9dfd32651513
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2023-06-30 14:26:20 +0200

    Localize main menu

--- Assets/Scripts/Misc/MainMenuUtils.cs
+++ Assets/Scripts/Misc/MainMenuUtils.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Localization.Settings;
 
 public class MainMenuUtils : MonoBehaviour
 {
     [SerializeField]
-    private GameObject newGameButton;
+    private GameObject mainPanel;
     [SerializeField]
-    private GameObject creditsButton;
-    [SerializeField]
-    private GameObject intro;
-    [SerializeField]
-    private GameObject startButton;
-
+    private GameObject introPanel;
     [SerializeField]
     private GameObject credits;
+
+    [SerializeField]
+    private Button localeButton;
+    [SerializeField]
+    private int currentLocale = 0;
+
+    private IEnumerator Start()
+    {
+        introPanel.SetActive(false);
+        mainPanel.SetActive(true);
+
+        yield return LocalizationSettings.InitializationOperation;
+
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[currentLocale];
+
+        localeButton.onClick.AddListener(ChangeLocale);
+    }
+
     public void LoadScene()
     {
         SceneManager.LoadScene("map", LoadSceneMode.Single);
@@ -29,10 +43,19 @@ public class MainMenuUtils : MonoBehaviour
 
     public void NewGame()
     {
-        newGameButton.SetActive(false);
-        creditsButton.SetActive(false);
+        mainPanel.SetActive(false);
         credits.SetActive(false);
-        startButton.SetActive(true);
-        intro.SetActive(true);
+        introPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeLocale()
+    {
+        currentLocale = (currentLocale + 1) % LocalizationSettings.AvailableLocales.Locales.Count;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[currentLocale];
     }
 }
